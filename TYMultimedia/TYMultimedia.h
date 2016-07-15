929f9a88f848dbe301d3a8312dce81aa540b2174
troy <siren.lb@alibaba-inc.com>
2016-07-15 15:35:40 +0800

    init
    Signed-off-by: troy <siren.lb@alibaba-inc.com>

---
+++ TYMultimedia/TYMultimedia.h
@@ -0,0 +1,19 @@
+//
+//  TYMultimedia.h
+//  TYMultimedia
+//
+//  Created by 李彬 on 16/7/15.
+//  Copyright © 2016年 李彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TYMultimedia.
+FOUNDATION_EXPORT double TYMultimediaVersionNumber;
+
+//! Project version string for TYMultimedia.
+FOUNDATION_EXPORT const unsigned char TYMultimediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TYMultimedia/PublicHeader.h>
+
+
